1c06ae9364a087d5792d6c4e65bd6277e907017d
Phamdinhthe250938 <phamdinhthe2509@gmail.com>
2022-09-07 23:45:25 +0700

    add phan chinh sua cv

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -16,4 +16,10 @@ export class UserService {
   saveCv(cvUser:any):Observable<any>{
     return this.http.post<any>(`http://localhost:8080/user/saveCvUser/`,cvUser);
   }
+  postDetail(id:number):Observable<any>{
+    return this.http.get<any>(`http://localhost:8080/user/postDetail/${id}`);
+  }
+  findCvByIdUser(id:number):Observable<any>{
+    return this.http.get<any>(`http://localhost:8080/user/findCvByIdUser/${id}`);
+  }
 }
